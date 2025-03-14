4dae1bfaea4a36ab1d945137acd8a13184de44c4
Carlos Gonzales Ramos <rcarlosgonzales98@gmail.com>
2025-03-14 12:22:25 -0500

    Creación del proyecto

---
+++ src/test/java/com/hacom/hacom_app/HacomAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hacom.hacom_app;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HacomAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
