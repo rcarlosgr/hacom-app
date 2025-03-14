e10c805c3c37b2fd0f5dd00b3b200594ba9c7998
Carlos Gonzales Ramos <rcarlosgonzales98@gmail.com>
2025-03-14 17:45:23 -0500

    log4j2

--- src/main/java/com/hacom/hacom_app/services/impl/TraceMsgServiceImpl.java
+++ src/main/java/com/hacom/hacom_app/services/impl/TraceMsgServiceImpl.java
@@ -6,6 +6,8 @@ import com.hacom.hacom_app.entities.TraceMsg;
 import com.hacom.hacom_app.repositories.TraceMsgRepository;
 import com.hacom.hacom_app.services.TraceMsgService;
 import lombok.RequiredArgsConstructor;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
 import org.bson.types.ObjectId;
 import org.springframework.stereotype.Service;
 import reactor.core.publisher.Flux;
@@ -17,23 +19,29 @@ import java.time.ZoneOffset;
 @Service
 @RequiredArgsConstructor
 public class TraceMsgServiceImpl implements TraceMsgService {
+    private static final Logger logger = LogManager.getLogger(TraceMsgServiceImpl.class);
+
     private final TraceMsgRepository repository;
 
     @Override
     public Mono<TraceMsgResponse> saveTraceMsg(TraceMsgRequest req) {
+        logger.info("Request recibido {}", req);
         TraceMsg traceMsg = TraceMsg.builder()
                 ._id(new ObjectId())
                 .sessionId(req.getSessionId())
                 .payload(req.getPayload())
                 .ts(req.getTs().toInstant())
                 .build();
-
-        return repository.save(traceMsg)
-                .map(this::toResponse);
+        logger.info("Entidad a gaurdar {}", traceMsg);
+        Mono<TraceMsg> saved = repository.save(traceMsg);
+        logger.info("Guardado en base de datos con éxito {}", saved);
+        return saved.map(this::toResponse);
     }
 
     @Override
     public Flux<TraceMsgResponse> getTraceMsgsByDateRange(OffsetDateTime from, OffsetDateTime to) {
+        logger.info("from recibido {}", from);
+        logger.info("to recibido {}", to);
         return repository.findByTsBetween(from.toInstant(), to.toInstant())
                 .map(this::toResponse);
     }
